0c81a9adf6f7b430e567ce3be5420c0a40c33f98
Cristianyelmo <cristianama2019@gmail.com>
2023-01-23 11:10:14 -0300

    carrito de la compra

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views", "index.html")))
 app.get("/login", (req, res) => res.sendFile(path.join(__dirname, "views", "login.html")))
 app.get("/productCart", (req, res) => res.sendFile(path.join(__dirname, "views", "productCart.html")))
-app.get("/register", (req, res) => res.sendFile(path.join(__dirname, "views", "register.html")))
+app.get("/productDetail", (req, res) => res.sendFile(path.join(__dirname, "views", "register.html")))
 app.get("/login", (req, res) => res.sendFile(path.join(__dirname, "views", "login.html")))
 
 app.listen(port, () => console.log(`Servidor funcionando en http://localhost:${port}`));
