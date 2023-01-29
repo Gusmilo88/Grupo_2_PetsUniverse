6b1c80af9a7f0f11c97a980f2ddb45a91ca06b84
Gus <gus_eze1988@hotmail.com>
2023-01-28 23:54:27 -0300

    Agrega estructura .ejs

--- app.js
+++ app.js
@@ -1,16 +1,41 @@
-const express = require("express");
-const app = express()
-const port = 3030;
-const path = require("path");
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-app.use(express.static(path.join(__dirname, "public")));
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views", "index.html")))
-app.get("/login", (req, res) => res.sendFile(path.join(__dirname, "views", "login.html")))
-app.get("/productCart", (req, res) => res.sendFile(path.join(__dirname, "views", "productCart.html")))
-app.get("/productDetail", (req, res) => res.sendFile(path.join(__dirname, "views", "productDetail.html")))
-app.get("/register", (req, res) => res.sendFile(path.join(__dirname, "views", "register.html")))
-app.get("/nosotros", (req, res) => res.sendFile(path.join(__dirname, "views", "nosotros.html")))
+var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.listen(port, () => console.log(`Servidor funcionando en http://localhost:${port}`));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
