fd47d6ac8757d7b2d3aab06f5d0e8ea29dc97c24
GabrielaMeza93 <gabrielameza1993@gmail.com>
2023-02-13 13:45:10 -0300

    agrego Crud producyCreate

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,23 +1,55 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/products.json')
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 module.exports = {
 
-    
-    
-    
-    productDetail: (req,res)=>{
-        return res.render('productDetail')
+    productDetail: (req, res) => {
+		
+        const {id}= req.params; 
+		const product = products.find(product => product.id === +id);
+        const mil = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");	
+        return res.render('productDetail',{
+            ...product,
+            products,        
+            mil		
+		})
+	},
+
+    productEdit: (req,res) => {
+        const {id} = req.params;
+        const product = products.find(product => product.id === +id);
+
+        return res.render("productEdit", {
+            ...product,
+        })
     },
 
-    productEdit: (req,res)=>{
-        return res.render('productEdit')
+    productUpdate: (req, res) => {
+        const {name, discount, price, description, category} = req.body;
     },
-
     productCreate: (req,res)=>{
         return res.render('productCreate')
     },
 
+    store: (req, res) => {
+        const {name, discount, price, description, category, weight, productTipe} = req.body;
+        const newProduct = {
+            id : products[products.length - 1].id + 1,
+            name : name.trim(),
+            description : description.trim(),
+            price : +price,
+            discount : +discount,
+            image : null,
+            weight : +weight,
+            productTipe : productTipe,
+            category
+        }
+        products.push(newProduct);
+        fs.writeFileSync(productsFilePath, JSON.stringify(products, null, 3), 'utf-8')
 
-    
-    
-  
-
+        return res.redirect('/')
+    }
 }
+
--- routes/product.js
+++ routes/product.js
@@ -1,14 +1,17 @@
 var express = require('express');
+const productController = require('../controllers/productController');
 var router = express.Router();
 
 /* GET home page. */
-const {productDetail,productEdit, productCreate} = require('../controllers/productController')
+const {productDetail,productEdit, productCreate, productUpdate, store} = require('../controllers/productController')
 
 
-router.get('/productDetail',productDetail) 
+router.get('/productDetail/:id',productDetail) 
 
-router.get('/productCreate',productCreate) 
+router.get('/productCreate/',productCreate)
+router.post('/productCreate', store) 
 
-router.get('/productEdit',productEdit) 
+router.get('/productEdit/:id', productEdit) 
+router.put('/productEdit/:id', productUpdate)
 
 module.exports = router;
