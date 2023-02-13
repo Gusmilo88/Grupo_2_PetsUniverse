acb2c30a84e13ccea2712bb9550e265d947f0fed
Cristianyelmo <cristianama2019@gmail.com>
2023-02-13 15:29:24 -0300

    iteracion en home y vista todos los productos con filtros acumulativos

--- routes/product.js
+++ routes/product.js
@@ -3,9 +3,9 @@ const productController = require('../controllers/productController');
 var router = express.Router();
 
 /* GET home page. */
-const {productDetail,productEdit, productCreate, productUpdate, store} = require('../controllers/productController')
+const {productDetail,productEdit, productCreate, productUpdate, store,productFilter} = require('../controllers/productController')
 
-
+router.get('/',productFilter)
 router.get('/productDetail/:id',productDetail) 
 
 router.get('/productCreate/',productCreate)
