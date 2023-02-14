3bbb11c2d12f3941c20191456c5a09b9ee1af141
Maxi <maxi.cuellar.2002@gmail.com>
2023-02-14 13:09:50 -0300

    metodo delete terminado

--- routes/product.js
+++ routes/product.js
@@ -3,7 +3,7 @@ const productController = require('../controllers/productController');
 var router = express.Router();
 
 /* GET home page. */
-const {productDetail,productEdit, productCreate, productUpdate, store,productFilter} = require('../controllers/productController')
+const {productDetail,productEdit, productCreate, productUpdate, store,productFilter, destroy} = require('../controllers/productController')
 
 router.get('/',productFilter)
 router.get('/productDetail/:id',productDetail) 
@@ -14,6 +14,8 @@ router.post('/productCreate', store)
 router.get('/productEdit/:id', productEdit) 
 router.put('/productEdit/:id', productUpdate)
 
-router.delete('/delete/:id', productController.destroy); 
+router.delete('/productDelete/:id', destroy), function (req, res){
+    res.send("hola")
+}; 
 
 module.exports = router;
