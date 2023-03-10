45236ae2d38785cc3b3c659e02192adc909aaed3
Gus <gus_eze1988@hotmail.com>
2023-03-10 17:05:05 -0300

    Agrega multer al register

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,9 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-const {login,register} = require('../controllers/userController')
+const {login,register} = require('../controllers/userController');
+const { uploadUserImage } = require('../middlewares/upload');
 
 router.get('/login', login) 
 router.get('/register', register) 
+router.post('/register', uploadUserImage.single("fotoPerfil"), register)
+
 
 module.exports = router;
