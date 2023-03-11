085a232502285c668162b2933809d2a2e7229986
Gus <gus_eze1988@hotmail.com>
2023-03-11 00:30:26 -0300

    Agrega validaciones backend, contraseña encriptada

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,12 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-const {login,register} = require('../controllers/userController');
+const {login,register, processRegister} = require('../controllers/userController');
 const { uploadUserImage } = require('../middlewares/upload');
+const registerUserValidator = require('../validations/registerUserValidator');
 
 router.get('/login', login) 
 router.get('/register', register) 
-router.post('/register', uploadUserImage.single("fotoPerfil"), register)
+router.post('/register', uploadUserImage.single("avatar"), registerUserValidator,processRegister)
 
 
 module.exports = router;
