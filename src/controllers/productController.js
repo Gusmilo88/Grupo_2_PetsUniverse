ae9f585e6fde64b3b7b671bb2d1bbe6913e3e7a6
Gus <gus_eze1988@hotmail.com>
2023-04-03 00:47:56 -0300

    Crea los archivos .env e inicio sequelize

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const db = require("../database/models")
 
 const productsFilePath = path.join(__dirname, '../data/products.json')
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
