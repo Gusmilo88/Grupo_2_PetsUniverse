144e324ec7f451dfac7a6b6b6e54a6980c8e5e58
Maxi <maxi.cuellar.2002@gmail.com>
2023-04-14 13:33:15 -0300

    crud fallido

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const path = require('path');
-const db = require("../database/models")
+const db = require("../database/models");
+const { promise } = require('bcrypt/promises');
+const { redirect } = require('express/lib/response');
+const { title } = require('process');
 
 
 const productsFilePath = path.join(__dirname, '../data/products.json')
@@ -8,86 +11,6 @@ const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const writeJson = (data = {}) =>fs.writeFileSync(path.join(__dirname, "../data/queries.json"),JSON.stringify(data),"utf-8");
 module.exports = {
-
-    productDetail: (req, res) => {
-		
-        const {id}= req.params; 
-		const product = products.find(product => product.id === +id);
-        const mil = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");	
-        return res.render('productDetail',{
-            ...product,
-            products,        
-            mil		
-		})
-	},
-
-    productEdit: (req,res) => {
-        const {id} = req.params;
-        const product = products.find(product => product.id === +id);
-
-        return res.render("productEdit", {
-            ...product,
-        })
-    },
-
-    productUpdate: (req, res) => {
-        const {id} = req.params
-        const product = products.find(product => product.id === +id);
-        const {name, description, price, discount, image, weight, category, productType, stock} = req.body;
-
-        const productModified = {
-            id : +id,
-			name : name.trim(),
-			description : description.trim(),
-			price : +price,
-			discount : +discount,
-			image : product.image,
-      weight : +weight,
-			category : category ? "perros" : "perros" || category ? "gatos" : "gatos",
-      productType : productType ? "alimento" : "alimento" || productType ? "juguetes" : "juguetes" || productType ? "salud" : "salud",
-      stock : +stock,
-		}
-
-        const productsModified = products.map(product => {
-            if(product.id === +id){
-                return productModified
-            }
-            return product;
-        })
-
-        fs.writeFileSync(productsFilePath, JSON.stringify(productsModified, null, 3), "utf-8");
-        return res.redirect("/products/productDetail/" + id)
-
-        
-    },
-
-    productCreate: (req,res)=>{
-        return res.render('productCreate')
-    },
-
-    store: (req, res) => {
-      const mil = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-        const {name, discount, price, description, category, weight, productType, stock, image} = req.body;
-
-        const newProduct = {
-            id : products[products.length - 1].id + 1,
-            name : name.trim(),
-            description : description.trim(),
-            price : +price,
-            discount : +discount,
-            image : null,
-            weight : +weight,
-            productType : productType ? "alimento" : "alimento" || productType ? "juguetes" : "juguetes" || productType ? "salud" : "salud",
-            category : category ? "perros" : "perros" || category ? "gatos" : "gatos",
-            stock : +stock,
-            mil
-            
-        }
-        products.push(newProduct);
-        fs.writeFileSync(productsFilePath, JSON.stringify(products, null, 3), 'utf-8')
-
-        return res.redirect('/')
-    },
 
     productFilterCats: (req,res)=>{
 
@@ -127,10 +50,6 @@ module.exports = {
 			  return product.productType === productType;
 			});
 		  }
-
-
-
-
 
         return res.render('productsCats',{
             products:allProducts,
@@ -187,15 +106,202 @@ module.exports = {
       })
   },
 
-    
-    destroy : (req, res) => {
-		// Do the magic
-		const {id} = req.params;
-		const productsModified = products.filter(product => product.id !== + id)
-		fs.writeFileSync(productsFilePath, JSON.stringify(productsModified, null, 3), "utf-8");
-		return res.redirect("/")
-	}
+/* ------------------------------ */
+
+  productCreate: (req,res)=>{
+    return res.render('productCreate')
+},
+
+store: (req, res) => {
+  const mil = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    const {name, discount, price, description, category, weight, productType, stock, image} = req.body;
+
+    const newProduct = {
+        id : products[products.length - 1].id + 1,
+        name : name.trim(),
+        description : description.trim(),
+        price : +price,
+        discount : +discount,
+        image : null,
+        weight : +weight,
+        productType : productType ? "alimento" : "alimento" || productType ? "juguetes" : "juguetes" || productType ? "salud" : "salud",
+        category : category ? "perros" : "perros" || category ? "gatos" : "gatos",
+        stock : +stock,
+        mil
+        
+    }
+    products.push(newProduct);
+    fs.writeFileSync(productsFilePath, JSON.stringify(products, null, 3), 'utf-8')
+
+    return res.redirect('/')
+},
+
+
+productCreate: (req,res)=>{
+    const {name, description, categoryId, productTypeId, price, discount, weight, stock, image} = req.body
+
+    db.Product.create({
+        ...req.body,
+        title : title.trim
+    })
+      .then(newProduct =>{
+        console.log(newProduct)
+        return res.redirect('/' + newProduct.id)
+      })
+      .catch(error => console.log(error))
+},
+/* ------------------------------ */
+
+/* productEdit: (req,res) => {
+  const {id} = req.params;
+  const product = products.find(product => product.id === +id);
+
+  return res.render("productEdit", {
+      ...product,
+  })
+}, */
+
+
+productEdit: function(req,res) {
+  const product = db.petsuniverse.finByPk(req.params.id)
+
+  Promise.all([product])
+  .then(([product])=>{
+    return res.send("productEdit"),{
+      Product : product
+    }
+  }).catch(error => console.log(error))
+},
+
+/* ------------------------------ */
+
+/* productUpdate: (req, res) => {
+  const {id} = req.params
+  const product = products.find(product => product.id === +id);
+  const {name, description, price, discount, image, weight, category, productType, stock} = req.body;
+
+  const productModified = {
+      id : +id,
+name : name.trim(),
+description : description.trim(),
+price : +price,
+discount : +discount,
+image : product.image,
+weight : +weight,
+category : category ? "perros" : "perros" || category ? "gatos" : "gatos",
+productType : productType ? "alimento" : "alimento" || productType ? "juguetes" : "juguetes" || productType ? "salud" : "salud",
+stock : +stock,
+}
+
+  const productsModified = products.map(product => {
+      if(product.id === +id){
+          return productModified
+      }
+      return product;
+  })
+
+  fs.writeFileSync(productsFilePath, JSON.stringify(productsModified, null, 3), "utf-8");
+  return res.redirect("/products/productDetail/" + id)
+}, */
+
+
+productUpdate: (req, res) => {
+    db.petsuniverse.update(
+      {
+        ...req.body
+      },
+      {
+        where : {
+          id : req.params.id
+        }
+      }
+    ).then(() => res.redirect("/products/productDetail/" + req.params.id))
+     .catch(error => console.log(error))
+},
+
+/* ------------------------------ */
+
+destroy : (req, res) => {
+  // Do the magic
+  const {id} = req.params;
+  const productsModified = products.filter(product => product.id !== + id)
+  fs.writeFileSync(productsFilePath, JSON.stringify(productsModified, null, 3), "utf-8");
+  return res.redirect("/")
+}
+};
+
+destroy : (req, res) => {
+  db.petsuniverse.destroy(
+    {
+      where : {
+        id : req.params.id
+      }
+    }
+  ).then(() => res.redirect("/products"))
+   .catch(error => console.log(error))
+}
+  
+
+/* ------------------------------ */
+
+productDetail: (req, res) => {
+		
+  const {id}= req.params; 
+const product = products.find(product => product.id === +id);
+  const mil = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");	
+  return res.render('productDetail',{
+      ...product,
+      products,        
+      mil		
+})
+};
+
+
+productDetail: (req, res) => {
+  const { id } = req.params;
+  
+  db.petsuniverse.findByPk(id,{
+    include : [
+      {
+        association : 'images',
+        attributes : ['name']
+      }
+    ]
+  })
+    .then(course => {
+      return res.render("productDetail", {
+        title: "Detalle del producto",
+        ...products.dataValues,
+      });
+    })
+    .catch(error => console.log(error))
 }
 
 
+
+/* ------------------------------ */
+
+
+
+
+
+
+list: (req, res) => {
+
+  db.Course.findAll({
+    where : {
+      visible : true
+    },
+    include : ['images']
+  })
+    .then(courses => {
+      return res.render("courses/list", {
+        title: "Lista de cursos",
+        courses
+      });
+    })
+    .catch(error => console.log(error))
+
+ 
+}
 
