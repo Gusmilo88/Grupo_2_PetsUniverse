00018d9f4b0aeca0b150981fd8ce9261a5ab2082
Maxi <maxi.cuellar.2002@gmail.com>
2023-04-17 12:05:43 -0300

    .

--- src/routes/product.js
+++ src/routes/product.js
@@ -6,15 +6,15 @@ var router = express.Router();
 const {productDetail,productEdit, productCreate, productUpdate, store, destroy,productFilterCats,productFilterDogs, search} = require('../controllers/productController');
 const checkUserAdmin = require('../middlewares/checkUserAdmin');
 
-router.get('/gatos',productFilterCats)
-router.get('/perros',productFilterDogs)
+/* router.get('/gatos',productFilterCats)
+router.get('/perros',productFilterDogs) */
 router.get('/productDetail/:id',productDetail) 
 
-router.get('/productCreate/',/* checkUserAdmin */productCreate)
-router.post('/productCreate', store)  
+/* router.get('/productCreate/',checkUserAdminproductCreate)
+router.post('/productCreate', store)  */ 
 
-router.get('/productEdit/:id',/* checkUserAdmin,  */productEdit) 
-router.put('/productEdit/:id', productUpdate)
+router.get('/productEdit/:id',checkUserAdmin,productEdit) 
+/* router.put('/productEdit/:id', productUpdate) */
 
 router.delete('/productDelete/:id', destroy), 
 router.get('/search',search)
