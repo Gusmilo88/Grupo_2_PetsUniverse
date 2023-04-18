d8566f2787caaf9629c93cf8a3b8f7edcce0e1c8
Cristianyelmo <cristianama2019@gmail.com>
2023-04-17 21:40:08 -0300

    vistas de productos

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -9,9 +9,11 @@ const { title } = require('process');
 /* const productsFilePath = path.join(__dirname, '../data/products.json')
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8')); */
 
-/* const writeJson = (data = {}) =>fs.writeFileSync(path.join(__dirname, "../data/queries.json"),JSON.stringify(data),"utf-8"); */
+ const writeJson = (data = {}) =>fs.writeFileSync(path.join(__dirname, "../data/queries.json"),JSON.stringify(data),"utf-8"); 
 module.exports = {
-  productFilterCats: (req, res) => {
+
+
+ /*  productFilterCats: (req, res) => {
     db.Product.findAll({
       where: {
         visible: true,
@@ -25,7 +27,7 @@ module.exports = {
         });
       })
       .catch((error) => console.log(error));
-  },
+  }, */
 
   productFilterDogs: (req, res) => {
     db.Product.findAll({
@@ -71,25 +73,52 @@ module.exports = {
   },
 
 productCreate: (req,res)=>{
-    db.Product.create({
-        ...req.body,
-        title : title.trim
+   const productoTipo = db.ProductType.findAll({
+   
+    attributes: ["name", "id"],
+  }); 
+ 
+  const categories = db.Category.findAll({
+   
+    attributes: ["name", "id"],
+  });
+
+  Promise.all([ productoTipo, categories])
+    .then(([productoTipo, categories]) => {
+      return res.render("productCreate", {
+        productoTipo,
+        categories,
+      });
     })
-      .then(newProduct =>{
-        console.log(newProduct)
-        return res.redirect('/' + newProduct.id)
-      })
-      .catch(error => console.log(error))
+    .catch((error) => console.log(error));
 },
 
 productEdit: function(req,res) {
-  const product = db.petsuniverse.finByPk(req.params.id)
 
-  Promise.all([product])
-  .then(([product])=>{
-    return res.send("productEdit"),{
-      Product : product
+  const { id } = req.params;
+  const producto = db.Product.findByPk(id,
+
+    {
+      include : [
+        {
+          association : 'categories',
+          attributes : ['name']
+        },
+        {
+          association : 'productTypes',
+          attributes : ['name']
+        },]
     }
+    
+    
+    )
+  
+  Promise.all(([ producto]))
+  
+  .then(([ producto])=>{
+    return res.render("productEdit",{
+      ...producto.dataValues
+    })
   }).catch(error => console.log(error))
 },
 
@@ -122,51 +151,206 @@ search : (req,res) => {
   return res.render('courses/results',{
     courses : []
   })
+},
+
+
+productFilterCats: (req,res)=>{
+
+  if (!req.query.price  && !req.query.productType) {
+writeJson();
+}
+  let queries = require("../data/queries.json");
+
+writeJson({ ...queries, ...req.query });
+
+const { price, category,productType } = { ...queries, ...req.query }  
+
+ProductsCat  = db.Product.findAll({
+  where:{
+
+      categoryId:2
+      
+      
+  }
+})
+
+Promise.all([ ProductsCat])
+
+
+.then(([ ProductsCat]) => {
+  return res.render("productsCats",{
+    products:ProductsCat,
+    queries: { ...queries, ...req.query }
+  });
+})
+.catch((error) => console.log(error));
+
+
+
+
+/* et allProducts = ProductsCat;
+
+     if (price) {
+// ordenar por el precio
+if (price === "min") {
+  allProducts = db.Product.findAll({
+    order: [
+      ["population", "DESC"],
+     
+    ],
+  })
+} 
+
+if (price === "max") {
+  allProducts = db.Product.findAll({
+    order: [
+      ["price", "ASC"],
+     
+    ],
+  })
+} 
+} 
+
+     if (productType) {
+allProducts = db.Product.findAll({
+  where:{
+
+      productTypeId:productType
+      
+      
+  }
+})
+}
+
+return res.render('productsCats',{
+  products:allProducts,
+  queries: { ...queries, ...req.query }
+}) */
+},
+
+
+productFilterDogs: (req,res)=>{
+
+  if (!req.query.price  && !req.query.productType) {
+writeJson();
+}
+  let queries = require("../data/queries.json");
+
+writeJson({ ...queries, ...req.query });
+
+const { price, category,productType } = { ...queries, ...req.query }  
+
+ProductsDog  = db.Product.findAll({
+  where:{
+
+      categoryId:1
+      
+      
+  }
+})
+
+Promise.all([ ProductsDog])
+
+
+.then(([ ProductsDog]) => {
+  return res.render("productsDogs",{
+    products:ProductsDog,
+    queries: { ...queries, ...req.query }
+  });
+})
+.catch((error) => console.log(error));
+
+
+
+
+/* et allProducts = ProductsCat;
+
+     if (price) {
+// ordenar por el precio
+if (price === "min") {
+  allProducts = db.Product.findAll({
+    order: [
+      ["price", "DESC"],
+     
+    ],
+  })
+} 
+
+if (price === "max") {
+  allProducts = db.Product.findAll({
+    order: [
+      ["price", "ASC"],
+     
+    ],
+  })
+} 
+} 
+
+     if (productType) {
+allProducts = db.Product.findAll({
+  where:{
+
+      productTypeId:productType
+      
+      
+  }
+})
+}
+
+return res.render('productsCats',{
+  products:allProducts,
+  queries: { ...queries, ...req.query }
+}) */
 }
 
 };
 
-/*     productFilterCats: (req,res)=>{
+/* productFilterCats: (req,res)=>{
 
-        if (!req.query.price  && !req.query.productType) {
-			writeJson();
-		  }
-        let queries = require("../data/queries.json");
-	  
-		  writeJson({ ...queries, ...req.query });
-	  
-		  const { price, category,productType } = { ...queries, ...req.query } 
-     
-      ProductsCat = products.filter((product) => {
-			  return product.category === "gatos";
-			})
+  if (!req.query.price  && !req.query.productType) {
+writeJson();
+}
+  let queries = require("../data/queries.json");
 
-		  let allProducts = ProductsCat;
+writeJson({ ...queries, ...req.query });
 
-          if (price) {
-			// ordenar por el precio
-			if (price === "min") {
-			  allProducts = allProducts.sort((before, after) => {
-				return before.price - after.price;
-			  });
-			} else {
-			  allProducts = allProducts.sort((before, after) => {
-				return after.price - before.price;
-			  });
-			}
-		  }
+const { price, category,productType } = { ...queries, ...req.query } 
 
-           if (productType) {
-			allProducts = allProducts.filter((product) => {
-			  return product.productType === productType;
-			});
-		  }
+ProductsCat = products.filter((product) => {
+  return product.category === "gatos";
+})
 
-      return res.render('productsCats',{
-        products:allProducts,
-        queries: { ...queries, ...req.query }
-      })
-    }, */
+let allProducts = ProductsCat;
+
+    if (price) {
+// ordenar por el precio
+if (price === "min") {
+  allProducts = allProducts.sort((before, after) => {
+  return before.price - after.price;
+  });
+} else {
+  allProducts = allProducts.sort((before, after) => {
+  return after.price - before.price;
+  });
+}
+}
+
+     if (productType) {
+allProducts = allProducts.filter((product) => {
+  return product.productType === productType;
+});
+}
+
+return res.render('productsCats',{
+  products:allProducts,
+  queries: { ...queries, ...req.query }
+})
+} */
+
+
+
+
+
 
 /*     productFilterDogs: (req,res)=>{
 
