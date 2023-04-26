26e5aa1d569b20625883110b07cbabaf48f53086
Cristianyelmo <cristianama2019@gmail.com>
2023-04-25 23:28:26 -0300

    crud productos

--- src/controllers/api/productsApiController.js
+++ src/controllers/api/productsApiController.js
@@ -1,5 +1,6 @@
-const {productsAll} = require('../../services/productsServices')
-
+const {productsAll,createProducts,productsDelete,ProductsUpdate} = require('../../services/productsServices')
+const createResponseError = require('../../helpers/createResponseError');
+const{validationResult} = require('express-validator')
 module.exports = {
 
 index:async(req,res)=>{
@@ -35,9 +36,137 @@ try {
 
 
 
-}
+},
 
 
+store:async(req,res)=>{
+        
+        
+        
+    try {
+    
+        const errors = validationResult(req)
+        
+          
+          
+        
+          
+        if(!errors.isEmpty()) throw{
+          status:400,
+          message:errors.mapped()
+        }
+
+        if (!req.file && !req.fileValidationError) {
+    errors.errors.push({
+      value: "",
+      msg: "El producto debe tener por lo menos una imagen",
+      param: "images",
+      location: "file",
+    });
+  }
+
+  if (req.fileValidationError) {
+    errors.errors.push({
+      value: "",
+      msg: req.fileValidationError,
+      param: "images",
+      location: "file",
+    });
+  }
+  
+    
+    
+        const newGenre = await createProducts(
+             req.body)
+       
+    
+        return res.status(200).json({
+            ok:true,
+            meta:{
+                
+                status:200,
+                
+                total:1},
+            data:newGenre,
+            url:`/api/genres/${newGenre.id}`
+            
+                })
+        
+    } catch (error) {
+        return  createResponseError(res,error)
+    }
+    
+    
+        },
+
+destroy: async(req, res) => {
+
+            try {
+            
+                const{
+                    params:{id}
+                } = req
+            
+              
+                const products = await  productsDelete(id)
+            
+                 return res.status(200).json({
+               
+            message:`se ha borrado el producto con el id ${id}`
+            
+                })
+            
+            
+                       
+                
+            } catch (error) {
+            
+                
+                return  createResponseError(res,error)
+                
+            }
+            
+            
+            
+                    
+                },
+
+
+
+update:async(req,res)=>{
+                    try {
+            
+                        const{
+                            params:{id}
+                        } = req
+                    
+                      
+                        const productsupdate = await  ProductsUpdate(req.body,id)
+                    
+                         return res.status(200).json({
+                       
+                    ok:true,
+                    meta:{
+                        
+                        status:200,
+                        
+                        total:1,
+                        url:`/api/genres/${id}`},
+                    data:productsupdate,
+                    
+                        })
+                    
+                    
+                               
+                        
+                    } catch (error) {
+                    
+                        
+                        return  createResponseError(res,error)
+                        
+                    }
+                },
+    
 
 
 
--- src/services/productsServices.js
+++ src/services/productsServices.js
@@ -1,7 +1,8 @@
+const { name } = require('ejs')
 const db = require('../database/models')
 const {literalQueryUrl,literalQueryUrlImage} = require('../helpers')
 
-module.exports ={
+module.exports = {
 
 
     productsAll: async(req)=>{
@@ -38,8 +39,103 @@ message: error.message
 
 
 
-    }
+    },
+    createProducts:async({name,description,price,discount,weight,productType,category,stock})=>{
 
+        try {
+           
+     
+
+            const newproducts=db.Product.create(
+               
+                
+                
+                
+                
+                {
+     
+                name,
+                description,
+                price,
+                discount,
+                
+                weight,
+                productTypeId : productType === "alimento" ? 1 : null || productType === "juguetes" ? 2 : null || productType === "salud" ? 3 : null,
+                categoryId : category === "perros" ? 1 : null ||category === "gatos" ?2 : null ,
+                stock,
+               
+            })
+return newproducts
+
+        } catch (error) {
+            throw {
+                status: 500,
+                message:error.message
+            }
+        }
+        
+
+
+
+
+    },
+
+
+    productsDelete :async (id)=>{
+                try {
+                
+                    
+            
+                    const productsdestroy = await   db.Product.destroy({
+                        where:{
+                            id
+                        }
+                    });
+                    return productsdestroy
+                } catch (error) {
+                    throw {
+                        status: 500,
+                        message:error.message
+                    }
+                }
+            },
+
+
+            ProductsUpdate :async ({name,description,price,discount,weight,productType,category,stock},id)=>{
+                try {
+                
+                    
+            
+                    const productupdate = await   db.Product.update({
+     
+                        name,
+                        description,
+                        price,
+                        discount,
+                        
+                        weight,
+                        productTypeId : productType === "alimento" ? 1 : null || productType === "juguetes" ? 2 : null || productType === "salud" ? 3 : null,
+                        categoryId : category === "perros" ? 1 : null ||category === "gatos" ?2 : null ,
+                        stock,
+                       
+                    },{
+                        where:{
+                            id
+                        }
+                    });
+                    return productupdate
+                } catch (error) {
+                    throw {
+                        status: 500,
+                        message:error.message
+                    }
+                }
+                
+                
+                
+                
+                        
+                    } ,
 
 
 
